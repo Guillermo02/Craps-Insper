531c9cfb42085064b537f6dfc37f824539cd7cc3
Guillermo02 <guigokuznietz@gmail.com>
2020-04-02 00:48:55 -0300

    primeiro commit

---
+++ Codigos Crapt Insper spider.py
@@ -0,0 +1,40 @@
+pergunta=input("Você gostaria de continuar apostando? Se sim digite (s) se não digite (n): ")
+    
+if pergunta == "n":
+    print("Obrigado por jogar!")
+
+elif pergunta == "s":
+    jogador=1000
+
+    #come_out
+    come_out= input("O que você deseja apostar? Opções: pass line bet, field, any craps ou twelve: ")
+        
+    if come_out=="pass line bet":
+        input("Quanto você deseja apostar?")
+        
+        
+        
+    elif come_out=="field":
+        input("Quanto você deseja apostar?")
+        
+        
+        
+    elif come_out=="any craps":
+        input("Quanto você deseja apostar?")
+        
+        
+        
+    elif come_out=="twelve":
+        input("Quanto você deseja apostar?")
+    
+    
+    
+    #import random
+    #dado1=random.randint(1,6)
+    #dado2=random.randint(1,6)
+    
+    #if jogador==0:
+        #print("Que pena você está sem crédito")
+ 
+   
+    
